6858cf933d12cfaab80e9cdd387bd6b146429c75
SuryaKommanapalli <suryakommanapalli80@gmail.com>
2023-06-16 17:25:37 +0000

    commit

---
+++ src/components/ProductsHeader/index.js
@@ -0,0 +1,44 @@
+import {BsFilterRight} from 'react-icons/bs'
+import {AiOutlineSearch} from 'react-icons/ai'
+
+import './index.css'
+
+const ProductsHeader = props => {
+  const onChangeSortby = event => {
+    const {changeSortby} = props
+    changeSortby(event.target.value)
+  }
+
+  const {sortbyOptions, activeOptionId} = props
+
+  return (
+    <div className="products-header">
+      <div className="input-container">
+        <input type="search" placeholder="Search" className="input" />
+        <AiOutlineSearch className="search-icon" />
+      </div>
+      <h1 className="products-list-heading">All Products</h1>
+      <div className="sort-by-container">
+        <BsFilterRight className="sort-by-icon" />
+        <p className="sort-by">Sort by</p>
+        <select
+          className="sort-by-select"
+          value={activeOptionId}
+          onChange={onChangeSortby}
+        >
+          {sortbyOptions.map(eachOption => (
+            <option
+              key={eachOption.optionId}
+              value={eachOption.optionId}
+              className="select-option"
+            >
+              {eachOption.displayText}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsHeader
